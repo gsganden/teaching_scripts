7e0b4762916d0cd74c7f3cfc1b2fe70cb8495f76
gsganden <greg@gandenberger.org>
2018-06-03 20:33:17 -0500

    Make a simple timer app

---
+++ timer.py
@@ -0,0 +1,25 @@
+import argparse
+
+import time
+
+from tqdm import tqdm
+
+
+def main(args):
+    num_seconds = 60**2 * args['hours'] + 60 * args['minutes'] + args['seconds']
+    for i in tqdm(range(num_seconds)):
+        time.sleep(1)
+    print('\a')
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--hours', required=False, default=0, type=int)
+    parser.add_argument('-m', '--minutes', required=False, default=0, type=int)
+    parser.add_argument('-s', '--seconds', required=False, default=0, type=int)
+    args = vars(parser.parse_args())
+    return args
+
+
+if __name__ == '__main__':
+    args = _parse_args()
+    main(args)
