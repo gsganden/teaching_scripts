61d6a16d186c81e16b11507113edaa68d7d8b268
gsganden <gsganden@gmail.com>
2020-03-29 09:59:21 -0500

    Remove dead code

--- scrub_notebook_solutions.py
+++ scrub_notebook_solutions.py
@@ -1,28 +1,15 @@
 """
-Usage: python scrub_notebook_scrubs.py <input_file> <output_file>
-
-where `input_file_asset_relpath` is the relative path from the input
-file to a directory containing assets (e.g. images) and
-`output_file_asset_relpath` is the relative path from the output file
-file to the same directory.
-
-Cleans Jupyter notebooks for sharing with students:
-
-- Clears outputs of all cells.
-- Clears contents of any cell that contains "/scrub/" from the line that
-contains it onwards. The idea is that you will mark cell contents that
-you don't want to share with students (e.g. solution code) with the
-comment "# /scrub/".
+Usage: `python scrub_notebook_solutions.py <input_file> <output_file>`
 """
-# coding: utf-8
 import json
 import sys
 
-INPUT_ASSET_DIR_RELPATH = '..'
-OUTPUT_ASSET_DIR_RELPATH = '.'
-
 
 def main(input_file, output_file):
+    """
+    Clean Jupyter notebooks for sharing with students by clearing all
+    outputs and the contents of any cell that has a "scrub" tag.
+    """
     with open(input_file, 'r') as f:
         notebook = json.load(f)
     for cell in notebook['cells']:
@@ -41,20 +28,6 @@ def _clear_outputs(cell):
     return cell
 
 
-def _get_scrub_comment_line_num(cell):
-    scrub_line_num = None
-    for line_num, line in enumerate(cell['source']):
-        if '/scrub/' in line:
-            scrub_line_num = line_num
-            break
-    return scrub_line_num
-
-
-def _clear_source_from_line(cell, line_num):
-    cell['source'] = cell['source'][:line_num]
-    return cell
-
-
 if __name__ == '__main__':
-    INPUT_FILE, OUTPUT_FILE = sys.argv[1:]
-    main(INPUT_FILE, OUTPUT_FILE)
+    input_file, output_file = sys.argv[1:]
+    main(input_file=input_file, output_file=output_file)
